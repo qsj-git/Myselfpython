67501faa59372c1bc0b355ed169b04850254bc5d
qiusj <152588qsj@sina.com>
2021-12-29 18:54:28 +0800

    xg

--- 类、面向对象/属性方法.py
+++ 类、面向对象/属性方法.py
@@ -1,15 +1,14 @@
 class Dog(object):
-    name = "zxc"
 
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, food):
+        self.__food = food
 
     @property  #属性方法：把一个方法变成一个静态属性
-    def eat(self):
-        print("%s is eating dd " % self.name)
+    def eat(self):    # getter 名
+        print("he is eating %s " % self.__food)
 
-    @eat.setter
-    def eat(self, food):
+    @eat.setter      # 这边的eat是getter名，要与getter名字保持一致
+    def eat(self, food):   # setter 名，要与getter名字保持一致
         print("set to food:", food)
         self.__food = food
 
@@ -17,3 +16,4 @@ class Dog(object):
 d = Dog("wer")
 d.eat
 d.eat = "apple"
+# d.eat
